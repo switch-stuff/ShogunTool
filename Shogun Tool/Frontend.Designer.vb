b4f5de38e1eb7dfa1b050d675ca8cbd3319c110a
simonmkwii <35590998+simonmkwii@users.noreply.github.com>
2018-05-10 13:45:51 +1000

    Version 3

---
+++ Shogun Tool/Frontend.Designer.vb
@@ -0,0 +1,236 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Arial", 36.0!)
+        Me.Button1.Location = New System.Drawing.Point(7, 102)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(673, 153)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Get Info!"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Arial", 12.0!)
+        Me.Label1.Location = New System.Drawing.Point(6, 13)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(123, 36)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Title ID:"
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.Font = New System.Drawing.Font("Arial", 12.0!)
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"US", "GB", "JP", "AU", "TW", "KR"})
+        Me.ComboBox1.Location = New System.Drawing.Point(570, 52)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(102, 44)
+        Me.ComboBox1.TabIndex = 4
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("Arial", 12.0!)
+        Me.TextBox1.Location = New System.Drawing.Point(12, 52)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(553, 44)
+        Me.TextBox1.TabIndex = 0
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Arial", 12.0!)
+        Me.Label2.Location = New System.Drawing.Point(563, 13)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(123, 36)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Region:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Enabled = False
+        Me.Label3.Location = New System.Drawing.Point(1178, 8)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(0, 25)
+        Me.Label3.TabIndex = 6
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Enabled = False
+        Me.TextBox3.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox3.Location = New System.Drawing.Point(687, 52)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(668, 44)
+        Me.TextBox3.TabIndex = 7
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Enabled = False
+        Me.Label4.Font = New System.Drawing.Font("Arial", 12.0!)
+        Me.Label4.Location = New System.Drawing.Point(681, 214)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(182, 36)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "Description:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Enabled = False
+        Me.Label5.Font = New System.Drawing.Font("Arial", 12.0!)
+        Me.Label5.Location = New System.Drawing.Point(682, 13)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(168, 36)
+        Me.Label5.TabIndex = 9
+        Me.Label5.Text = "Game title:"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Enabled = False
+        Me.PictureBox1.Location = New System.Drawing.Point(7, 261)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(668, 374)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 10
+        Me.PictureBox1.TabStop = False
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Enabled = False
+        Me.TextBox4.Font = New System.Drawing.Font("Arial", 12.0!)
+        Me.TextBox4.Location = New System.Drawing.Point(687, 591)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(668, 44)
+        Me.TextBox4.TabIndex = 11
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Enabled = False
+        Me.Label6.Font = New System.Drawing.Font("Arial", 12.0!)
+        Me.Label6.Location = New System.Drawing.Point(680, 552)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(213, 36)
+        Me.Label6.TabIndex = 12
+        Me.Label6.Text = "Release Date:"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Enabled = False
+        Me.Label7.Font = New System.Drawing.Font("Arial", 12.0!)
+        Me.Label7.Location = New System.Drawing.Point(683, 114)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(156, 36)
+        Me.Label7.TabIndex = 14
+        Me.Label7.Text = "Publisher:"
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Enabled = False
+        Me.TextBox5.Font = New System.Drawing.Font("Arial", 12.0!)
+        Me.TextBox5.Location = New System.Drawing.Point(688, 159)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(668, 44)
+        Me.TextBox5.TabIndex = 13
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Font = New System.Drawing.Font("Arial", 12.0!)
+        Me.RichTextBox1.Location = New System.Drawing.Point(688, 256)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(667, 293)
+        Me.RichTextBox1.TabIndex = 15
+        Me.RichTextBox1.Text = ""
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1365, 647)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Shogun Tool"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents RichTextBox1 As RichTextBox
+End Class
---
+++ Shogun Tool/Frontend.vb
@@ -0,0 +1,186 @@
+﻿Imports System.IO
+Imports System.IO.File
+Imports System.Net
+Imports System.Security.Cryptography.X509Certificates
+Imports System.Security
+Imports System.Net.Security
+Imports Newtonsoft.Json.Linq
+Imports Newtonsoft.Json
+
+Public Class Form1
+    ' Allow self-signed certificates
+    Public Function AcceptAllCertifications(ByVal sender As Object, ByVal certification As System.Security.Cryptography.X509Certificates.X509Certificate, ByVal chain As System.Security.Cryptography.X509Certificates.X509Chain, ByVal sslPolicyErrors As System.Net.Security.SslPolicyErrors) As Boolean
+        Return True
+    End Function
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ' Enable the controls
+        PictureBox1.Enabled = True
+        RichTextBox1.Enabled = True
+        TextBox3.Enabled = True
+        TextBox4.Enabled = True
+        TextBox5.Enabled = True
+        Label3.Enabled = True
+        Label4.Enabled = True
+        Label5.Enabled = True
+        Label6.Enabled = True
+        Label7.Enabled = True
+
+        Try
+            ' Makes sure both boxes are filled
+            If TextBox1.Text = "" Or ComboBox1.Text = "" Then
+
+                MsgBox("Please enter a title ID and select a region!")
+
+            Else
+                ' Send cert-signed HTTPS GET requests to shogun
+                ' Force no certificate validation
+                ServicePointManager.ServerCertificateValidationCallback = AddressOf AcceptAllCertifications
+                ' Define the "TID" variable as the text in TextBox1 (The title ID field)
+                Dim TID As String = TextBox1.Text
+                ' Define the "REG" variable as the region chosen from the combo box
+                Dim REG As String = ComboBox1.Text
+                ' Generate the ID pair URL based on inputs
+                Dim GetIDPair As String = "https://bugyo.hac.lp1.eshop.nintendo.net/shogun/v1/contents/ids?shop_id=4&lang=en&country=" + REG + "&type=title&title_ids=" + TID
+                ' Use the cert "ShopN.p12" with the password "shop"
+                Dim Cert As New X509Certificate2("ShopN.p12", "shop")
+                ' Create the web request
+                Dim request As HttpWebRequest = WebRequest.Create(GetIDPair)
+                ' Add the cert to the request
+                request.ClientCertificates.Add(Cert)
+                ' Get the response
+                Dim response As HttpWebResponse = request.GetResponse
+                ' Parse the response as a text stream
+                Dim Rd As StreamReader = New StreamReader(response.GetResponseStream)
+                ' Read the entire response
+                Dim IDPair As String = Rd.ReadToEnd
+                ' Return the NSU ID only and write it to Label3
+                Label3.Text = IDPair.Substring(19, 14)
+                ' Define the NSU ID as Label3's text
+                Dim NSUID As String = Label3.Text
+                ' Generate the JSON URL based on inputs
+                Dim GetInfo As String = "https://bugyo.hac.lp1.eshop.nintendo.net/shogun/v1/titles/" + NSUID + "?shop_id=4&lang=en&country=" + REG
+                ' Create the web request
+                Dim request2 As HttpWebRequest = WebRequest.Create(GetInfo)
+                ' Add the cert to the request
+                request2.ClientCertificates.Add(Cert)
+                ' Get the response
+                Dim response2 As HttpWebResponse = request2.GetResponse
+                ' Parse the response as a text stream
+                Dim Rd2 As StreamReader = New StreamReader(response2.GetResponseStream)
+                ' Read the entire response
+                Dim Info As String = Rd2.ReadToEnd
+                ' Parse the returned JSON
+                Dim jResults As JObject = JObject.Parse(Info)
+                ' Check to see if the title is public
+                If jResults("public_status").ToString() = "public" Then
+                    ' Gets the game name from the "formal_name" object
+                    TextBox3.Text = jResults("formal_name").ToString()
+                    ' Gets the description from the "description" object
+                    RichTextBox1.Text = jResults("description").ToString()
+                    ' Gets the release date from the "release_date_on_eshop" object
+                    TextBox4.Text = jResults("release_date_on_eshop").ToString()
+                    ' Gets the publisher name from the "name" field in the "publisher" object
+                    TextBox5.Text = jResults("publisher")("name").ToString()
+                    ' Gets the image ID from the "hero_banner_url" object
+                    Dim IMGID As String = jResults("hero_banner_url").ToString()
+                    ' Generate the full image URL.
+                    Dim GetImg As String = "https://bugyo.hac.lp1.eshop.nintendo.net/" + IMGID + "?w=640"
+                    ' Opens a request to the URL
+                    Dim request3 As HttpWebRequest = WebRequest.Create(GetImg)
+                    ' Add certificate to request
+                    request3.ClientCertificates.Add(Cert)
+                    ' Get the response
+                    Dim response3 As HttpWebResponse = request3.GetResponse
+                    ' Parse the response, reading as binary data
+                    Dim Rd3 As BinaryReader = New BinaryReader(response3.GetResponseStream)
+                    ' Save the image data as temp.jpg
+                    WriteAllBytes("temp.jpg", Rd3.ReadBytes(8008135))
+                    ' Display the saved image the picture box
+                    PictureBox1.ImageLocation = "temp.jpg"
+                    Try
+                        ' If colours are defined, set form and controls to them
+                        ' Read colours from the "dominant_colors" object
+                        Dim GetBC = jResults("dominant_colors")(0).ToString()
+                        Dim NewBC = ColorTranslator.FromHtml("#" + GetBC)
+                        Dim GetFC = jResults("dominant_colors")(1).ToString()
+                        Dim NewFC = ColorTranslator.FromHtml("#" + GetFC)
+                        Dim GetSC = jResults("dominant_colors")(2).ToString()
+                        Dim NewSC = ColorTranslator.FromHtml("#" + GetSC)
+                        Me.BackColor = NewBC
+                        Label1.ForeColor = NewFC
+                        Label2.ForeColor = NewFC
+                        Label3.ForeColor = NewFC
+                        Label4.ForeColor = NewFC
+                        Label5.ForeColor = NewFC
+                        Label6.ForeColor = NewFC
+                        Label7.ForeColor = NewFC
+                        Button1.ForeColor = NewFC
+                        TextBox1.ForeColor = NewFC
+                        TextBox3.ForeColor = NewFC
+                        TextBox4.ForeColor = NewFC
+                        TextBox5.ForeColor = NewFC
+                        RichTextBox1.ForeColor = NewFC
+                        ComboBox1.ForeColor = NewFC
+                        TextBox1.BackColor = NewBC
+                        TextBox3.BackColor = NewBC
+                        TextBox4.BackColor = NewBC
+                        TextBox5.BackColor = NewBC
+                        RichTextBox1.BackColor = NewBC
+                        Button1.BackColor = NewBC
+                        ComboBox1.BackColor = NewBC
+                    Catch NoColor As System.NullReferenceException
+                        ' If there's a title with no colour, reset all to default
+                        Me.BackColor = DefaultBackColor
+                        Label1.ForeColor = DefaultForeColor
+                        Label2.ForeColor = DefaultForeColor
+                        Label3.ForeColor = DefaultForeColor
+                        Label4.ForeColor = DefaultForeColor
+                        Label5.ForeColor = DefaultForeColor
+                        Label6.ForeColor = DefaultForeColor
+                        Label7.ForeColor = DefaultForeColor
+                        Button1.ForeColor = DefaultForeColor
+                        TextBox1.ForeColor = DefaultForeColor
+                        TextBox3.ForeColor = DefaultForeColor
+                        TextBox4.ForeColor = DefaultForeColor
+                        TextBox5.ForeColor = DefaultForeColor
+                        RichTextBox1.ForeColor = DefaultForeColor
+                        ComboBox1.ForeColor = DefaultForeColor
+                        TextBox1.BackColor = DefaultBackColor
+                        TextBox3.BackColor = DefaultBackColor
+                        TextBox4.BackColor = DefaultBackColor
+                        TextBox5.BackColor = DefaultBackColor
+                        RichTextBox1.BackColor = DefaultBackColor
+                        Button1.BackColor = DefaultBackColor
+                        ComboBox1.BackColor = DefaultBackColor
+                    End Try
+
+                Else
+                    ' If it returns a non-public or cartridge-only title (e.g. Labo, Skylanders, etc.)
+                    MsgBox("This title exists, but isn't listed on the eShop.")
+                End If
+            End If
+        Catch ex As System.ArgumentOutOfRangeException
+            ' If it detects an ID ending in "800", assume it's an update and replace it with "000"
+            If TextBox1.Text.Substring(13, 3) = "800" Then
+                MsgBox("This is an update ID!" + vbNewLine + "Because I'm nice, I'll give you the base title ID." + vbNewLine + "BUT DON'T DO THIS AGAIN!!!")
+                TextBox1.Text = TextBox1.Text.Remove(13, 3)
+                TextBox1.AppendText("000")
+            Else
+                MsgBox("Invalid input! Please check the title ID and region.")
+            End If
+        End Try
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Checks to make sure the cert exists
+        If File.Exists("ShopN.p12") Then
+        Else
+            MsgBox("Please put the eShop certificate (ShopN.p12) in this folder.")
+            Close()
+        End If
+    End Sub
+    Private Sub Form1_FormClosed(sender As Object, e As FormClosedEventArgs) Handles MyBase.FormClosed
+        ' Delete the temporary image when the program is closed
+        File.Delete("temp.jpg")
+    End Sub
+End Class
